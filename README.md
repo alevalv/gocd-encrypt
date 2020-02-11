33bad7e159c607ea7162eb600b64d47009103b7a
Alejandro Valdes <alejandrovaldes@live.com>
2020-02-10 23:11:21 -0500

    Removed tokyo
    Switched to blocking client
    Added optional password option that disables password prompt

---
+++ README.md
@@ -0,0 +1,5 @@
+# gocd-encrypt
+
+This is a CLI program to encrypt a random string using the 
+[gocd encryption api](https://api.gocd.org/current/#encryption), it can authenticate the server using
+basic authentication or do an anonymous request with `-a`.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 extern crate reqwest;
-extern crate tokio;
 extern crate serde_json;
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut options = getopts::Options::new();
     options.optflag("a", "anonymous-request", "set the petition to be anonymous");
@@ -14,13 +12,13 @@ async fn main() -> Result<(), reqwest::Error> {
     let matches = match options.parse(&args[1..]) {
         Ok(m) => { m }
         Err(_) => {
-            print!("{}", options.short_usage("gocd-encrypt"));
+            println!("{}", options.short_usage("gocd-encrypt"));
             std::process::exit(1);
         }
     };
     let string_to_encrypt = matches.opt_str("i");
     let server = matches.opt_str("s").unwrap() + "/go/api/admin/encrypt";
-    let mut request = reqwest::Client::new()
+    let mut request = reqwest::blocking::Client::new()
         .post(&server)
         .header(reqwest::header::ACCEPT, "application/vnd.go.cd.v1+json")
         .header(reqwest::header::CONTENT_TYPE, "application/json")
@@ -32,16 +30,22 @@ async fn main() -> Result<(), reqwest::Error> {
             eprintln!("username missing");
             std::process::exit(1);
         }
-        let username = matches.opt_str("u");
-        let password = rpassword::prompt_password_stderr("Password: ").unwrap();
-        request = request.basic_auth(username.unwrap().clone(), Some(password.clone()))
+        let username = matches.opt_str("u").unwrap();
+        let password: String;
+        if matches.opt_present("p") {
+            password = matches.opt_str("p").unwrap();
+        } else {
+            password = rpassword::prompt_password_stderr("Password: ").unwrap();
+        };
+        request = request.basic_auth(username.clone(), Some(password.clone()));
     }
-    let echo_json: serde_json::Value =
-        request.send()
-        .await?
-        .json()
-        .await?;
+    let echo_json: serde_json::Value = request.send().unwrap().json().unwrap();
+    if echo_json.get("encrypted_value") == None {
+        eprintln!("Received response is invalid");
+        std::process::exit(1);
+    }
+    else {
+        println!("{}", echo_json.get("encrypted_value").unwrap().as_str().unwrap());
+    }
 
-    println!("{}", echo_json.get("encrypted_value").unwrap().as_str().unwrap());
-    Ok(())
 }
